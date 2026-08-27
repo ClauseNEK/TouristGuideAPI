629d10148c298919bc2f1cc48667282cb9e6f551
ClauseNEK <nicl10000@stud.ek.dk>
2026-08-27 13:58:39 +0200

    Attraction: Attributter: Constructor Getters & Setters override

--- src/main/java/com/example/touristguideapi/model/TouristAttraction.java
+++ src/main/java/com/example/touristguideapi/model/TouristAttraction.java
@@ -3,5 +3,37 @@ package com.example.touristguideapi.model;
 public class TouristAttraction {
     private String name;
     private String description;
+
+    //Constructor (maybe not need)
+    public TouristAttraction(String name, String description){
+        this.name = name;
+        this.description = description;
+    }
+
+    // Getters \\
+    public String getName(){
+        return name;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+
+    // Setters if needed \\
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setDescription(String Description){
+        this.description = Description;
+    }
+
+
+    //toString override
+    @Override
+    public String toString() {
+        return "Navn:" + name + "Beskrivelse" + description;
+    }
 }
 
