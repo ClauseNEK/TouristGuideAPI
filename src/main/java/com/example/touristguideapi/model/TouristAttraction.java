9d45a09e1fe7376a5481967bcf085c9d548acea5
ClauseNEK <nicl10000@stud.ek.dk>
2026-08-27 13:21:49 +0200

    Initial commit

---
+++ src/main/java/com/example/touristguideapi/model/TouristAttraction.java
@@ -0,0 +1,7 @@
+package com.example.touristguideapi.model;
+
+public class TouristAttraction {
+    private String name;
+    private String description;
+}
+
---
+++ src/main/java/com/example/touristguideapi/repository/TouristRepository.java
@@ -0,0 +1,5 @@
+package com.example.touristguideapi.repository;
+
+public class TouristRepository {
+
+}
