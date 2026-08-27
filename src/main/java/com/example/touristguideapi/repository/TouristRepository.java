342fa9f0906ea9765799a7853ba94a882c6c3848
ChefenML <milo1000@stud.ek.dk>
2026-08-27 14:01:11 +0200

    First draft

--- src/main/java/com/example/touristguideapi/repository/TouristRepository.java
+++ src/main/java/com/example/touristguideapi/repository/TouristRepository.java
@@ -1,5 +1,51 @@
 package com.example.touristguideapi.repository;
 
+import com.example.touristguideapi.model.TouristAttraction;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+Opret klassen TouristRepository i repository package med annoteringen
+@Repository.
+Tilføj en ArrayList til opbevaring af data (om ikke så længe skal I arbejde med en
+rigtig database). Opret et par TouristAttraction objekter, som tilføjes til denne
+ArrayList.
+Klassen skal desuden indeholde CRUD metoder, der arbejder på ovenstående
+ArrayList. Vent evt. med den endelige metodesignatur for CRUD metoderne til I
+har set beskrivelsen af Controller klassens endpoints nedenfor.
+ */
+@Repository
 public class TouristRepository {
+    ArrayList<TouristAttraction> list = new ArrayList<>();
+
+    public TouristRepository(){
+        list.add(new TouristAttraction("Tivoli","Sjov for hele familien"));
+        list.add(new TouristAttraction("Bakken","Sjov for hele familien"));
+        list.add(new TouristAttraction("Vega","Musik året rundt"));
+        list.add(new TouristAttraction("Royal Arena","Koncerter mm."));
+    }
+
+    public List<TouristAttraction> getAllAttractions(){
+        return list;
+    }
+
+    public void addAttraction(TouristAttraction TA){
+        list.add(TA);
+    }
+
+    public TouristAttraction getNumberAttraction(int number){
+        return list.get(number);
+    }
+
+    public void deleteAttractionNumber(int number){
+        list.remove(number);
+    }
+
+    public void updateAttrations(int number, TouristAttraction TA){
+        list.set(number,TA);
+    }
+
 
 }
