f63c52bcbcf0cfb99fea201d0034b2cca3eb29b0
Jeppeoberg <jeob1000@stud.ek.dk>
2026-09-01 21:08:28 +0200

    Controller lavet med basal url uden endpoints

--- src/main/java/com/example/touristguideapi/controller/TouristController.java
+++ src/main/java/com/example/touristguideapi/controller/TouristController.java
@@ -1,4 +1,30 @@
 package com.example.touristguideapi.controller;
 
+import com.example.touristguideapi.model.TouristAttraction;
+import com.example.touristguideapi.service.TouristService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("attractions")
 public class TouristController {
+
+    private final TouristService touristService;
+
+    public TouristController(TouristService touristService) {
+        this.touristService = touristService;
+    }
+
+    //alt skal gå igennem service og der skal ikke være repository herinde
+
+    @GetMapping
+    @ResponseBody
+    public ResponseEntity<List<TouristAttraction>> getAttractions() {
+        List<TouristAttraction> attractions = touristService.getAllAttractions();
+        return ResponseEntity.ok(attractions);
+    }
+
 }
