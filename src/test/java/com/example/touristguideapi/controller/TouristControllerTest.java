ddaf8c5b3d0737b19c84938cdb512c20cd946a52
ChefenML <milo1000.stud.ek.dk>
2026-09-23 11:59:30 +0200

    CityTags test...

--- src/test/java/com/example/touristguideapi/controller/TouristControllerTest.java
+++ src/test/java/com/example/touristguideapi/controller/TouristControllerTest.java
@@ -7,6 +7,13 @@ import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import org.springframework.test.web.servlet.MockMvc;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -23,13 +30,18 @@ class TouristControllerTest {
 
 //test for at url give forventede view/string whatevs
     @Test
-    void getAttractions() throws Exception {
+    void getAttractionsTest() throws Exception {
     mockMvc.perform(get("/attractions")).
             andExpect(status().isOk()).
             andExpect(view().name("attractionList"));
     }
 
     @Test
-    void getTags() {
+    void getAttractionCityTagsTest() {
+        final List<String> cities = Arrays.asList(
+                "Albertslund", "København", "Odense", "Kongens Lyngby", "Aarhus");
+        assertEquals(service.getCities(),cities);
+
     }
+
 }
