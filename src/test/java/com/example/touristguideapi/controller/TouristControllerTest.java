f314dd5429dcc9e728edb9f4dba94dd0861fdc6b
ChefenML <milo1000@stud.ek.dk>
2026-09-25 08:58:50 +0200

    flere test

--- src/test/java/com/example/touristguideapi/controller/TouristControllerTest.java
+++ src/test/java/com/example/touristguideapi/controller/TouristControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.touristguideapi.controller;
 
+import com.example.touristguideapi.model.TouristAttraction;
 import com.example.touristguideapi.repository.TouristRepository;
 import com.example.touristguideapi.service.TouristService;
 import org.junit.jupiter.api.Test;
@@ -10,11 +11,13 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -44,6 +47,26 @@ class TouristControllerTest {
         TouristRepository tr = new TouristRepository();
         assertEquals(tr.getCities(),cities);
 
+    }
+
+    @Test
+    void getAllAttrationsTest(){
+        when(service.getAllAttractions()).thenReturn(getAttractions());
+    }
+
+    public List<TouristAttraction> getAttractions(){
+        List<TouristAttraction> list = new ArrayList<>();
+        list.add(new TouristAttraction("Tivoli", "Sjov for hele familien", "København",
+                List.of("Børnevenlig", "Underholdning"), "tivoli.png"));
+        list.add(new TouristAttraction("Bakken", "Underholdning for store og for små", "Kongens Lyngby",
+                List.of("Børnevenlig", "Underholdning"), "bakken_logo_2026.png"));
+        list.add(new TouristAttraction("Vega", "Musik året rundt", "København",
+                List.of("Underholdning"), "vega.png"));
+        list.add(new TouristAttraction("Royal Arena", "Koncerter mm.", "København",
+                List.of("Underholdning"), "royalarena.png"));
+        list.add(new TouristAttraction("TuristInformation", "Hjælp til alle dine turist behov", "København",
+                List.of("Gratis"), "turistinformation.png"));
+    return list;
     }
 
 }
